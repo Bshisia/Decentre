6506aa4892199d25de0bfd1ce0b64f4f0a4ef677
bshisia <shisiabrian7@gmail.com>
2025-09-01 22:03:52 +0300

    Add deployment script for Certificate contract

---
+++ decentre/src/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const Certificate = await ethers.getContractFactory("Certificate");
+    const certificate = await Certificate.deploy();
+
+    await certificate.waitForDeployment();
+
+    console.log("Certificate contract deployed to:", await certificate.getAddress());
+}
+
+// Execute the deployment script
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
