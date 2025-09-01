63e0f54b6a52aa16def1fcf11336810ec12455f0
bshisia <shisiabrian7@gmail.com>
2025-09-01 22:01:48 +0300

    Add Home page component with Chakra UI layout

---
+++ decentre/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Box p={8}>
+      <Heading>Decentre - Decentralized Certification System</Heading>
+    </Box>
+  )
+}
