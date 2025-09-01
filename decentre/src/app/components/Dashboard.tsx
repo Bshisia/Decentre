a414ba3b5c4dd7bd2f35f6e135d933f2854adaaa
bshisia <shisiabrian7@gmail.com>
2025-09-01 22:02:09 +0300

    Add Dashboard component to display overview of issued certificates and user actions

---
+++ decentre/src/app/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Overview of issued certificates and user actions.</p>
+            {/* Additional components and functionalities can be added here */}
+        </div>
+    );
+};
+
+export default Dashboard;
