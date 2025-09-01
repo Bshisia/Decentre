f7fd991aeb9cd23fb9cafe0f4e5ec86745f81b97
bshisia <shisiabrian7@gmail.com>
2025-09-01 22:01:01 +0300

    Add Hardhat configuration for Sepolia network and account task

---
+++ decentre/hardhat.config.ts
@@ -0,0 +1,22 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: SEPOLIA_PRIVATE_KEY ? [`0x${SEPOLIA_PRIVATE_KEY}`] : [],
+    },
+  },
+};
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
